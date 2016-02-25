5fd5ba0be21efc06d7278ff6273d0767d6bf796c
AD <ad@ADs-Mac-mini.local>
2016-02-25 10:15:08 -0600

    Create form to upload file and handle upload operation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,19 @@ class HomeController < ApplicationController
   end
 
   def upload
-  	binding.pry
+  	@upload_file = params['file']
+  	@file_name = @upload_file.original_filename
+  	@file_type = @upload_file.content_type
+
+  	## Upload to public/upload folder
+  	File.open(Rails.root.join('public', 'upload', @file_name), 'wb') do |file|
+  		file.write(@upload_file.read)
+  		
+  	end
+
+  	flash[:notice] = "Successfully upload #{@file_name}"
+  	render 'index'
+
   end
 
 
