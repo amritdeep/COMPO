88b1929dc53e16bd5fe6bc9d1f1ed6e8a128a918
AD <ad@ADs-Mac-mini.local>
2016-02-25 17:16:15 -0600

    store document value in document model

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
+require 'csv'
+require 'FileUtils'
+
 class HomeController < ApplicationController
   def index
     @document = Document.all
@@ -13,35 +16,58 @@ class HomeController < ApplicationController
     	@file_type = @upload_file.content_type
 
       check_file_extention_and_upload(@upload_file, @file_type, @file_name)
-    	## Save to Document Model
-  		@document = Document.new(name: @file_name, type: @file_type) 
-      check_doc_extention(@document)
-
-      respond_to do |format|
-        if @document.save
-          flash[:notice] = "Successfully upload #{@file_name}"
-        else
-          # flash[:notice] = "#{@file_name} cannot upload.Only CSV file are upload"
-          flash[:notice] = "#{@file_name} already exits."
+      
+      @doc_arry.each do |doc|
+        if doc.count == 2
+          @document = Document.new
+          @document.type = doc[0] 
+          @document.name = doc[1]
+          @document.save
         end
-         format.html { redirect_to root_path }
       end
-    else
-      flash[:notice] = "Please upload the file"
+
       redirect_to root_path
+
+    	## Save to Document Model
+  		# @document = Document.new(name: @file_name, type: @file_type) 
+    #   check_doc_extention(@document)
+
+    #   binding.pry
+
+    #   respond_to do |format|
+    #     if @document.save
+    #       flash[:notice] = "Successfully upload #{@file_name}"
+    #     else
+    #       flash[:notice] = "#{@file_name} already exits."
+    #     end
+    #      format.html { redirect_to root_path }
+    #   end
+    # else
+    #   flash[:notice] = "Please upload the file"
+    #   redirect_to root_path
     end
 
   end
 
   private
 
-  def check_doc_extention(document)
-    if document.type == "text/csv" && document.name.slice(-3..-1) == "csv"
-      document.type = "CSV"
-    else
-      document.type = ""
+  def check_column(file_name)
+    binding.pry
+    @doc_arry = []
+    file_path = "public/upload/#{file_name}"
+    @csv_file = CSV.read(file_path)
+    @csv_file.each do |i|
+      @doc_arry << i.compact
     end
   end
+
+  # def check_doc_extention(document)
+  #   if document.type == "text/csv" && (document.name.slice(-3..-1) == "csv" || )
+  #     document.type = "CSV"
+  #   else
+  #     document.type = ""
+  #   end
+  # end
 
   def check_file_extention_and_upload(files, type, name)
     ## Check the extention
@@ -51,8 +77,13 @@ class HomeController < ApplicationController
       File.open(Rails.root.join('public', 'upload', name), 'wb') do |file|
         file.write(files.read)
       end
-    end
-    
+
+    else
+      binding.pry
+      files.close
+    end 
+
+    check_column(name)    
   end
 
 end
