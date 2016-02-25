9c7f9c6bfa2c36ab9cd32d0d03bbfbc5780310b4
AD <ad@ADs-Mac-mini.local>
2016-02-25 14:40:19 -0600

    Create method to check the extention and upload

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,22 +12,19 @@ class HomeController < ApplicationController
     	@file_name = @upload_file.original_filename
     	@file_type = @upload_file.content_type
 
-    	## Upload to public/upload folder
-    	File.open(Rails.root.join('public', 'upload', @file_name), 'wb') do |file|
-    		file.write(@upload_file.read)
-    	end
-
+      check_file_extention_and_upload(@upload_file, @file_type, @file_name)
     	## Save to Document Model
-  		@document = Document.new(name: @file_name, type: @file_type)
+  		@document = Document.new(name: @file_name, type: @file_type) 
+      check_doc_extention(@document)
 
       respond_to do |format|
         if @document.save
           flash[:notice] = "Successfully upload #{@file_name}"
         else
-          flash[:notice] = "#{@file_name} already exits"
+          # flash[:notice] = "#{@file_name} cannot upload.Only CSV file are upload"
+          flash[:notice] = "#{@file_name} already exits."
         end
          format.html { redirect_to root_path }
-
       end
     else
       flash[:notice] = "Please upload the file"
@@ -36,7 +33,26 @@ class HomeController < ApplicationController
 
   end
 
+  private
 
+  def check_doc_extention(document)
+    if document.type == "text/csv" && document.name.slice(-3..-1) == "csv"
+      document.type = "CSV"
+    else
+      document.type = ""
+    end
+  end
 
+  def check_file_extention_and_upload(files, type, name)
+    ## Check the extention
+    if type == "text/csv" && name.slice(-3..-1) == "csv"
+      puts "Ready to go"
+      ## Handle the upload
+      File.open(Rails.root.join('public', 'upload', name), 'wb') do |file|
+        file.write(files.read)
+      end
+    end
+    
+  end
 
 end
---
+++ lib/scraper.rb
@@ -0,0 +1,15 @@
+# /lib/scraper.rb
+require 'rubygems'
+require 'pry'
+require 'csv'
+require 'zip'
+
+
+module Scraper
+	
+
+	def csv_file
+		binding.pry
+	end
+
+end
