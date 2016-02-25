68df85f41340affa19c427888190e13a4bdf984b
AD <ad@ADs-Mac-mini.local>
2016-02-25 09:36:32 -0600

    generate document model with name and type attritubes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,10 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def upload
+  	binding.pry
+  end
+
+
 end
---
+++ spec/models/document_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Document, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "should have name"
+  it "should have type"
+
+end
