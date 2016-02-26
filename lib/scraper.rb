e7e9a0de0620aefd06857376b6b210c061ea8f68
AD <ad@ADs-Mac-mini.local>
2016-02-26 09:24:10 -0600

    Create scraper module to handle file operation

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,14 +2,43 @@
 require 'rubygems'
 require 'pry'
 require 'csv'
-require 'zip'
+# require 'zip'
 
 
 module Scraper
-	
+  def check_column(files, file_typ, file_name)
+    @doc_arry = []
+    if file_typ == "text/csv" && file_name.slice(-3..-1) == "csv"    
+      file_path = "public/upload/#{file_name}"
+      @csv_file = CSV.read(file_path)
+      @csv_file.each do |i|
+        @doc_arry << i.compact
+      end
+    else
+      files.close
+      puts "File is close"
+    end
+  end
 
-	def csv_file
-		binding.pry
-	end
+  # def check_doc_extention(document)
+  #   if document.type == "text/csv" && (document.name.slice(-3..-1) == "csv" || )
+  #     document.type = "CSV"
+  #   else
+  #     document.type = ""
+  #   end
+  # end
+
+  def check_file_extention_and_upload(files, type, name)
+    ## Check the extention
+    if type == "text/csv" && name.slice(-3..-1) == "csv"
+      puts "Ready to go"
+      ## Handle the upload
+      File.open(Rails.root.join('public', 'upload', name), 'wb') do |file|
+        file.write(files.read)
+      end
+    else
+      puts "Its not CSV file. Its #{type} file"
+    end 
+  end
 
 end
