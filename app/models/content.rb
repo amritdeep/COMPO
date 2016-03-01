37d063b46a63dc8176821ea3ce977e5530b5b338
Amrit Deep <amrit@cratebind.com>
2016-03-01 13:07:16 -0600

    Preparing to upload in S3 Sever

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,6 +1,5 @@
 class Content < ActiveRecord::Base
   belongs_to :document
-
   
   has_attached_file :document, styles: {thumbnail: "60x60#"}
   validates_attachment :document, content_type: { content_type: "application/pdf" }  
